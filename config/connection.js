011d42e257835331989dc374f572b3c062321ccc
Davis Sjoberg <59490187+iron318davis@users.noreply.github.com>
2020-06-30 22:36:02 -0500

    EVEN MORE MOR SQL

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,18 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  
-  host: process.env.HOST,
-    port: process.env.PORT,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.JAWSDBURL
-    
-  //  host: "localhost",
-  //  port: 3306,
-  //  user: "root",
-  //  password: "Secret2020$",
-  //  database: "burgers_db"
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Secret2020$",
+    database: "burgers_db"
+ });
+}
+
 
 connection.connect(function(err) {
   if (err) {
