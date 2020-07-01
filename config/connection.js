ff4404d99c690c875b266a2d705422d1b290dcd8
Davis Sjoberg <59490187+iron318davis@users.noreply.github.com>
2020-06-30 21:55:49 -0500

    added SQL info

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Secret2020$",
-  database: "burgers_db"
+  host: "localhost", process.env.HOST,
+  port: 3306, process.env.PORT,
+  user: "root",process.env.USER,
+  password: "Secret2020$",process.env.PASSWORD,
+  database: "burgers_db", process.env.DATABASE
 });
 
 connection.connect(function(err) {
