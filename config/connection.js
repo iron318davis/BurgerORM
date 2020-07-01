04a1a31738e3cf06ecafa982f9b400e07bb44833
Davis Sjoberg <59490187+iron318davis@users.noreply.github.com>
2020-06-30 22:22:08 -0500

    maybe sql done

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  /*  
+  
   host: process.env.HOST,
     port: process.env.PORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE
-    */
-   host: "localhost",
-   port: 3306,
-   user: "root",
-   password: "Secret2020$",
-   database: "burgers_db"
+    
+  //  host: "localhost",
+  //  port: 3306,
+  //  user: "root",
+  //  password: "Secret2020$",
+  //  database: "burgers_db"
 });
 
 connection.connect(function(err) {
